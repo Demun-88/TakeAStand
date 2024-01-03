dbedd89904424090e27d04284c413df8653bd4db
Demun-88 <123742279+Demun-88@users.noreply.github.com>
2024-01-03 11:50:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use((req,res,next) => {
-<<<<<<< HEAD
     res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
     res.setHeader("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
     res.setHeader("Access-Control-Allow-Headers", 'Content-Type,Origin,X-Requested-With,Content-Length,cookie');
