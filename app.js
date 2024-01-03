c5a88ad8d93e7b4649efccdd7dd6d271ce7831e4
Demun-88 <123742279+Demun-88@users.noreply.github.com>
2024-01-03 11:50:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
     res.setHeader("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
     res.setHeader("Access-Control-Allow-Headers", 'Content-Type,Origin,X-Requested-With,Content-Length,cookie');
-    res.setHeader("Access-control-Allow-Credentials", 'true');
+    res.setHeader("Access-Control-Allow-Credentials", 'true');
 =======
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
