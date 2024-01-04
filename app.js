0fdfea8cdb97b08c1fd202769952cbbeb0ef0ade
Demun-88 <123742279+Demun-88@users.noreply.github.com>
2024-01-05 00:46:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.setHeader("Access-Control-Allow-Origin", 'https://relaxed-croissant-80ac32.netlify.app');
     res.setHeader("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
     res.setHeader("Access-Control-Allow-Headers", 'Content-Type,Origin,X-Requested-With,Content-Length,cookie');
     res.setHeader("Access-Control-Allow-Credentials", 'true');
